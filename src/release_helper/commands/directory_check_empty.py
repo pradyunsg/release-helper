76d425156705f993a16f18fc4218fc23150ec5a4
Pradyun Gedam <pradyunsg@users.noreply.github.com>
2020-08-20 23:25:31 +0530

    Implement directory-check-empty

--- src/release_helper/commands/directory_check_empty.py
+++ src/release_helper/commands/directory_check_empty.py
@@ -1,6 +1,54 @@
+"""Ensure a directory is empty, if it exists."""
+
+import os
+import shutil
+import sys
+
 import click
 
 
+def _delete_contents(path):
+    for root, dirs, files in os.walk(path):
+        for f in files:
+            os.unlink(os.path.join(root, f))
+        for d in dirs:
+            shutil.rmtree(os.path.join(root, d))
+
+
+def _handle_one(path, delete):
+    if not os.path.exists(path):
+        return
+
+    if not os.path.isdir(path):
+        return "is not a directory"
+
+    contents = os.listdir(path)
+    if not contents:  # nothing inside, all OK
+        return
+
+    if not delete:  # this has files, and not supposed to delete them.
+        return "is not empty"
+
+    _delete_contents(path)
+
+
 @click.command()
-def cmd():
-    raise NotImplementedError()
+@click.option(
+    "--delete",
+    is_flag=True,
+    default=False,
+    help="Delete the contents of given directories, if non-empty.",
+)
+@click.argument("directories", required=True, nargs=-1)
+def cmd(directories, delete):
+    """Ensure a directory is empty, if it exists."""
+    issues = []
+    for path in directories:
+        reason = _handle_one(path, delete)
+        if reason is not None:
+            issues.append((path, reason))
+
+    if issues:
+        for path, reason in issues:
+            click.echo(f"{path} {reason}.", err=True)
+        sys.exit(1)
---
+++ tests/test_directory_check_empty.py
@@ -0,0 +1,101 @@
+from pathlib import Path
+
+from click.testing import CliRunner
+from pytest import fixture
+
+from release_helper.commands.directory_check_empty import cmd
+
+
+@fixture(scope="function")
+def runner():
+    value = CliRunner(mix_stderr=False)
+    with value.isolated_filesystem():
+        yield value
+
+
+def test_with_does_not_exist(runner):
+    result = runner.invoke(cmd, ["folder"])
+
+    assert result.exit_code == 0
+    assert result.stdout == ""
+    assert result.stderr == ""
+
+
+def test_with_file(runner):
+    Path("file").write_text("I exist.\n")
+
+    result = runner.invoke(cmd, ["file"])
+
+    assert result.exit_code == 1
+    assert result.stdout == ""
+    assert result.stderr == "file is not a directory.\n"
+
+
+def test_with_empty(runner):
+    Path("folder").mkdir()
+
+    result = runner.invoke(cmd, ["folder"])
+
+    assert result.exit_code == 0
+    assert result.stdout == ""
+    assert result.stderr == ""
+
+
+def test_with_non_empty_with_one_file(runner):
+    Path("folder").mkdir()
+    Path("folder/file.txt").write_text("I exist.\n")
+
+    result = runner.invoke(cmd, ["folder"])
+
+    assert result.exit_code == 1
+    assert result.stdout == ""
+    assert result.stderr == "folder is not empty.\n"
+
+
+def test_with_non_empty_with_one_file_and_delete(runner):
+    Path("folder").mkdir()
+    Path("folder/file.txt").write_text("I exist.\n")
+
+    result = runner.invoke(cmd, ["folder", "--delete"])
+
+    assert result.exit_code == 0
+    assert result.stdout == ""
+    assert result.stderr == ""
+    assert not Path("folder/file.txt").exists()
+
+
+def test_with_non_empty_with_many_files(runner):
+    Path("folder").mkdir()
+    Path("folder/file.txt").write_text("I exist.\n")
+    Path("folder/file2.txt").write_text("And me too.\n")
+
+    result = runner.invoke(cmd, ["folder"])
+
+    assert result.exit_code == 1
+    assert result.stdout == ""
+    assert result.stderr == "folder is not empty.\n"
+
+
+def test_with_two_empty_directories(runner):
+    Path("folder-1").mkdir()
+    Path("folder-2").mkdir()
+
+    result = runner.invoke(cmd, ["folder-1", "folder-2"])
+
+    assert result.exit_code == 0
+    assert result.stdout == ""
+    assert result.stderr == ""
+
+
+def test_with_four_directories_with_two_non_empty(runner):
+    Path("folder-1").mkdir()
+    Path("folder-2").mkdir()
+    Path("folder-3").mkdir()
+    Path("folder-1/file.txt").write_text("I exist.\n")
+    Path("folder-3/file.txt").write_text("I exist.\n")
+
+    result = runner.invoke(cmd, ["folder-1", "folder-2", "folder-3", "folder-4"])
+
+    assert result.exit_code == 1
+    assert result.stdout == ""
+    assert result.stderr == ("folder-1 is not empty.\n" "folder-3 is not empty.\n")
