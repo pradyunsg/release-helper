6638bc9ac359e72be11722b587c848fd1ef41ca4
Pradyun Gedam <pradyunsg@users.noreply.github.com>
2020-08-21 16:21:30 +0530

    Implement version-bump

--- src/release_helper/commands/version_bump.py
+++ src/release_helper/commands/version_bump.py
@@ -1,6 +1,36 @@
+import re
+import sys
+from pathlib import Path
+
 import click
 
 
 @click.command()
-def cmd():
-    raise NotImplementedError()
+@click.argument(
+    "file",
+    required=True,
+    type=click.Path(
+        exists=True, file_okay=True, dir_okay=False, writable=True, readable=True
+    ),
+)
+@click.argument("version_string", required=True)
+def cmd(file, version_string):
+    """Bump version in given file.
+
+    If this is a `.py` file, replace the version in a __version__ = "..." assignment.
+    If this is a `.txt` file, write the version with a newline into the file.
+    Errors out otherwise.
+    """
+    path = Path(file)
+    if path.suffix == ".py":
+        # Find and replace the __version__ = "..." line.
+        contents = path.read_text()
+        new_contents = re.sub(
+            '__version__ = "(.+)"', f'__version__ = "{version_string}"', contents
+        )
+        path.write_text(new_contents)
+    elif path.suffix == ".txt":
+        path.write_text(version_string + "\n")
+    else:
+        click.echo(f"Don't know what to do for {path.suffix} files")
+        sys.exit(1)
---
+++ tests/test_version_bump.py
@@ -0,0 +1,52 @@
+from click.testing import CliRunner
+from pytest import fixture
+
+from release_helper.commands.version_bump import cmd
+
+
+@fixture(scope="function")
+def runner():
+    value = CliRunner(mix_stderr=False)
+    with value.isolated_filesystem():
+        yield value
+
+
+def test_with_python_file(runner):
+    with open("foo.py", "w") as f:
+        f.writelines([
+            '"""String!"""\n',
+            '__version__ = "1.0.0"\n',
+        ])
+    result = runner.invoke(cmd, ["foo.py", "1.1.0"])
+
+    print(result.stdout)
+    print(result.stderr)
+    assert result.exit_code == 0
+    assert result.stdout == ""
+    assert result.stderr == ""
+
+    with open("foo.py") as f:
+        lines = f.readlines()
+
+    assert lines == [
+        '"""String!"""\n',
+        '__version__ = "1.1.0"\n',
+    ]
+
+
+def test_with_txt_file(runner):
+    with open("version.txt", "w") as f:
+        f.write("1.0.0")
+
+    result = runner.invoke(cmd, ["version.txt", "1.1.0"])
+
+    print(result.stdout)
+    print(result.stderr)
+    assert result.exit_code == 0
+    assert result.stdout == ""
+    assert result.stderr == ""
+
+    with open("version.txt") as f:
+        contents = f.read()
+
+    assert contents == "1.1.0\n"
