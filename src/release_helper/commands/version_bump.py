7007d905ab6d43d4dc78f7e62077dd98ce6f82b5
Pradyun Gedam <pradyunsg@users.noreply.github.com>
2022-04-29 15:10:01 +0100

    Allow version-bump to take a `--variable-name` to bump

--- src/release_helper/commands/version_bump.py
+++ src/release_helper/commands/version_bump.py
@@ -13,11 +13,16 @@ import click
         exists=True, file_okay=True, dir_okay=False, writable=True, readable=True
     ),
 )
+@click.option(
+    "--variable-name",
+    default="__version__",
+    type=str,
+)
 @click.argument("version_string", required=True)
-def cmd(file, version_string):
+def cmd(file, variable_name, version_string):
     """Bump version in given file.
 
-    If this is a `.py` file, replace the version in a __version__ = "..." assignment.
+    If this is a `.py` file, replace the version in an assignment of the given version.
     If this is a `.txt` file, write the version with a newline into the file.
     Errors out otherwise.
     """
@@ -26,11 +31,16 @@ def cmd(file, version_string):
         # Find and replace the __version__ = "..." line.
         contents = path.read_text()
         new_contents = re.sub(
-            '__version__ = "(.+)"', f'__version__ = "{version_string}"', contents
+            f'{variable_name} = "(.+)"',
+            f'{variable_name} = "{version_string}"',
+            contents,
         )
+        if new_contents == contents:
+            click.echo("Did not make any changes to the file.", err=True)
+            sys.exit(1)
         path.write_text(new_contents)
     elif path.suffix == ".txt":
         path.write_text(version_string + "\n")
     else:
-        click.echo(f"Don't know what to do for {path.suffix} files")
+        click.echo(f"Don't know what to do for {path.suffix} files", err=True)
         sys.exit(1)
--- tests/test_version_bump.py
+++ tests/test_version_bump.py
@@ -1,6 +1,22 @@
 from release_helper.commands.version_bump import cmd
 
 
+def test_with_txt_file(runner):
+    with open("version.txt", "w") as f:
+        f.write("1.0.0")
+
+    result = runner.invoke(cmd, ["version.txt", "1.1.0"])
+
+    assert result.exit_code == 0
+    assert result.stdout == ""
+    assert result.stderr == ""
+
+    with open("version.txt") as f:
+        contents = f.read()
+
+    assert contents == "1.1.0\n"
+
+
 def test_with_python_file(runner):
     with open("foo.py", "w") as f:
         f.writelines(['"""String!"""\n', '__version__ = "1.0.0"\n'])
@@ -19,17 +35,48 @@ def test_with_python_file(runner):
     ]
 
 
-def test_with_txt_file(runner):
-    with open("version.txt", "w") as f:
-        f.write("1.0.0")
-
-    result = runner.invoke(cmd, ["version.txt", "1.1.0"])
+def test_with_python_file_custom_var(runner):
+    with open("foo.py", "w") as f:
+        f.writelines(['"""String!"""\n', 'fun_version = "1.0.0"\n'])
+    result = runner.invoke(cmd, ["foo.py", "1.1.0", "--variable-name", "fun_version"])
 
     assert result.exit_code == 0
     assert result.stdout == ""
     assert result.stderr == ""
 
-    with open("version.txt") as f:
-        contents = f.read()
+    with open("foo.py") as f:
+        lines = f.readlines()
 
-    assert contents == "1.1.0\n"
+    assert lines == [
+        '"""String!"""\n',
+        'fun_version = "1.1.0"\n',
+    ]
+
+
+def test_with_python_file_no_change(runner):
+    with open("foo.py", "w") as f:
+        f.writelines(['"""String!"""\n', '__version__ = "1.0.0"\n'])
+    result = runner.invoke(cmd, ["foo.py", "1.0.0"])
+
+    assert result.exit_code == 1
+    assert result.stdout == ""
+    assert result.stderr == "Did not make any changes to the file.\n"
+
+    with open("foo.py") as f:
+        lines = f.readlines()
+
+    assert lines == [
+        '"""String!"""\n',
+        '__version__ = "1.0.0"\n',
+    ]
+
+
+def test_with_unknown_file(runner):
+    with open("README.rst", "w") as f:
+        f.write("This project is foobar 1.0.0")
+
+    result = runner.invoke(cmd, ["README.rst", "1.1.0"])
+
+    assert result.exit_code == 1
+    assert result.stdout == ""
+    assert result.stderr == "Don't know what to do for .rst files\n"
