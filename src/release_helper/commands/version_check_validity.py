8aa8fe86a57c23f10e2a493d0e5b9176645dc3f4
Pradyun Gedam <pradyunsg@users.noreply.github.com>
2022-04-29 16:49:34 +0100

    Add ability to require canonical versions

--- src/release_helper/commands/version_check_validity.py
+++ src/release_helper/commands/version_check_validity.py
@@ -1,15 +1,27 @@
 import sys
 
 import click
+from packaging.utils import canonicalize_version
 from packaging.version import InvalidVersion, Version
 
 
 @click.command()
 @click.argument("version", required=True)
-def cmd(version):
+@click.option("--require-canonical", is_flag=True)
+def cmd(version, require_canonical):
     """Ensure a version is a valid PEP 440 version."""
     try:
-        Version(version)
+        parsed_version = Version(version)
     except InvalidVersion:
         click.echo(f"{version!r} is not a valid PEP 440 version.", err=True)
         sys.exit(1)
+
+    if require_canonical:
+        canonical_version = canonicalize_version(parsed_version)
+        if version != canonical_version:
+            click.echo(
+                f"{version!r} is not a canonical PEP 440 version "
+                f"(should be {canonical_version!r}).",
+                err=True,
+            )
+            sys.exit(1)
--- tests/test_version_check_validity.py
+++ tests/test_version_check_validity.py
@@ -9,6 +9,24 @@ def test_with_valid(runner):
     assert result.stderr == ""
 
 
+def test_with_valid_canonical(runner):
+    result = runner.invoke(cmd, ["--require-canonical", "1.1"])
+
+    assert result.exit_code == 0
+    assert result.stdout == ""
+    assert result.stderr == ""
+
+
+def test_with_invalid_canonical(runner):
+    result = runner.invoke(cmd, ["--require-canonical", "1.2.b1"])
+
+    assert result.exit_code == 1
+    assert result.stdout == ""
+    assert result.stderr == (
+        "'1.2.b1' is not a canonical PEP 440 version (should be '1.2b1').\n"
+    )
+
+
 def test_with_invalid(runner):
     result = runner.invoke(cmd, ["fluffy"])
 
